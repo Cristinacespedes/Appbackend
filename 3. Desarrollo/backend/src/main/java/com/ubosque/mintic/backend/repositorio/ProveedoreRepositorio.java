3da994d0d9d4989b584ea24383188e58f9608e9d
cesar <contreras.cesar53@gmail.com>
2021-10-08 20:33:55 -0500

    borrar codigo para actualizar

--- 3. Desarrollo/backend/src/main/java/com/ubosque/mintic/backend/repositorio/ProveedoreRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.ubosque.mintic.backend.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ubosque.mintic.backend.entidad.Proveedore;
-
-public interface ProveedoreRepositorio extends CrudRepository<Proveedore, String>{
-
-}
