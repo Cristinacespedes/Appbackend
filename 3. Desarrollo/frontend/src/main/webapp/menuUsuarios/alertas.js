aaff435943adedc69846cd629c1720f84379142d
cesar <contreras.cesar53@gmail.com>
2021-10-13 01:52:01 -0500

    Actualizacion de backend y frontend:  incluye alerts que indican que campos requeridos segun la funcion a desarrollar. alert de confirmacion en evento borrar. (SOLO DEL MENU DE USUARIOS POR AHORA) avance modulo productos

---
+++ 3. Desarrollo/frontend/src/main/webapp/menuUsuarios/alertas.js
@@ -0,0 +1,48 @@
+function usuarioCreado() {
+	alert("Usuario Creado");
+}
+
+function errorCrear() {
+	alert("Faltan datos del usuario");
+}
+
+function confirmacionBorrar(e) {
+	var resultado = window.confirm('Estas seguro de borrar al usuario?');
+	
+	if(!resultado){
+		e.preventDefault() ;
+            returnToPreviousPage();
+		return false;
+	} return resultado;
+}
+
+function confirmacionCampos(e){
+	var txtUsuario = document.getElementById("txtUsuario").value;
+	if(txtUsuario==null || txtUsuario==""){
+		alert("Campo usuario requerido");
+		e.preventDefault() ;
+            returnToPreviousPage();
+		return false;
+	}
+	var txtNombre = document.getElementById("txtNombre").value;
+	if(txtNombre==null  || txtNombre==""){
+		alert("Campo nombre requerido");
+		e.preventDefault() ;
+            returnToPreviousPage();
+		return false;
+		}
+	var pwdContrasena = document.getElementById("pwdContrasena").value;
+	if(pwdContrasena==null  || pwdContrasena==""){
+		alert("Campo contraseña requerido");
+		e.preventDefault() ;
+            returnToPreviousPage();
+		return false;
+		}
+	var emCorreo = document.getElementById("emCorreo").value;
+	if(emCorreo==null  || emCorreo==""){
+		alert("Campo correo requerido");
+		e.preventDefault() ;
+            returnToPreviousPage();
+		return false;
+		}
+}
